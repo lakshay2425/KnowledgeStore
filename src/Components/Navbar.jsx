c63db7d6438fe0674b7712bece8483108eb3c76b
Rohan Kumar Singh <rohankrsingh125@gmail.com>
2024-03-26 22:19:10 +0530

    Navbar Modifications

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,61 +1,54 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
-
+import { Menu } from '@headlessui/react'
+import './Navbar.css'
 
 const Navbar = () => {
   return (
     <>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div className="container-fluid">
-    <Link className="navbar-brand" to="/">Book-Rental</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="navbar navbar-expand-lg" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item dropdown">
-          <Link className="nav-link dropdown-toggle" to="/categories" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Categories
-          </Link>
-          <ul className="dropdown-menu">
-            <li><Link className="dropdown-item" to="/Finance">Finance</Link></li>
-            <li><Link className="dropdown-item" to="/SkillBased">Skill-Based</Link></li>
-            <li><Link className="dropdown-item" to="/Biography">Biography</Link></li>
-            <li><Link className="dropdown-item" to="/SelfHelp">Self-Help</Link></li>
-            <li><Link className="dropdown-item" to="/Fictional">Fictional</Link></li>
-          </ul>
-        </li>
-      </ul>
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item dropdown">
-          <Link className="nav-link dropdown-toggle" to="" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Ways to Contact
-          </Link>
-          <ul className="dropdown-menu">
-            <li><Link className="dropdown-item" to="/Login">Login</Link></li>
-            <li><Link className="dropdown-item" to="/Signup">Signup</Link></li>
-            <li><Link className="dropdown-item" to="/feedback">Feedback</Link></li>
-            <li><Link className="dropdown-item" to="/suggestion">Suggestion</Link></li>
-            <li><Link className="dropdown-item" to="/Contact">Contact</Link></li>
-          </ul>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Enter the book name" aria-label="Enter the book name you want to search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-      <ul className='d-flex mb-2'>
-        <li className="nav-item list-unstyled mx-2">
-          <Link className="nav-link"  to="/Cart">Cart</Link>
-        </li>
-        <li className="nav-item list-unstyled mx-2">
-          <Link className="nav-link"  to="/Account">Account</Link>
-        </li>
-      </ul>
-    </div>
-  </div>
-    </nav>
+      <header>
+        <nav className='navbar'>
+          <div id="logo">Books.Rent</div>
+
+          
+            <div className="nav-links">
+              <a href="/" className="">
+                Books
+              </a>
+              <Menu>
+                <Menu.Button>Categories</Menu.Button>
+                <Menu.Items>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        className={`${active && 'bg-blue-500'}`}
+                        href="/account-settings"
+                      >
+                        Account settings
+                      </a>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        className={`${active && 'bg-blue-500'}`}
+                        href="/account-settings"
+                      >
+                        Documentation
+                      </a>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item disabled>
+                    <span className="opacity-75">Invite a friend (coming soon!)</span>
+                  </Menu.Item>
+                </Menu.Items>
+              </Menu>
+              <a><Link>Wishlist</Link></a>
+              <a><Link>About Us</Link></a>
+            </div>
+          
+        </nav>
+      </header>
     </>
   )
 }
