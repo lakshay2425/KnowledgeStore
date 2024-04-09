d01849aee6e8ea110b248b352177c3af8cd76880
VIKAS GULIA <153659071+VIKASGULIA17@users.noreply.github.com>
2024-04-09 19:11:23 +0530

    form changes

--- src/Components/Features/Form/Contact.jsx
+++ src/Components/Features/Form/Contact.jsx
@@ -1,56 +1,104 @@
-import React, {useState} from 'react'
-
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from "react";
+import "./form.css";
 const Contact = () => {
   const [details, setDetails] = useState({
-    username : '',
-    gmail : '',
-    number : '',
-    concern : ''
+    username: "",
+    gmail: "",
+    number: "",
+    concern: "",
   });
   const handleInputChange = (e) => {
     setDetails((currData) => {
-      return{
-        ...currData, [e.target.name] : e.target.value
-      }
-    })
-  }
+      return {
+        ...currData,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
     setDetails({
-        username : '',
-        gmail : '',
-        number : '',
-       concern : ''
-      })
-  }
+      username: "",
+      gmail: "",
+      number: "",
+      concern: "",
+    });
+  };
   return (
     <>
-     <form method="post" onSubmit={handleSubmit}>
-      <table>
-        <tr>
-          <td><label htmlFor="username">Username</label></td>
-          <td><input type="text" id='username'  onChange={handleInputChange} placeholder='Enter your username' name='username' value={details.username} /></td>
-        </tr>
-        <tr>
-          <td><label htmlFor="gmail">Gmail</label></td>
-          <td><input type="gmailt" id='gmail' onChange={handleInputChange} placeholder='Enter your gmail' name='gmail' value={details.gmail} /></td>
-        </tr>
-        <tr>
-          <td><label htmlFor="contact">Contact Number</label></td>
-          <td><input type="number" id='contact' onChange={handleInputChange} placeholder='Enter your contact number' name='number' value={details.number} /></td>
-        </tr>
-        <tr>
-          <td><label htmlFor="concern">Concern</label></td>
-          <td><textarea name="concern" id="concern" onChange={handleInputChange} cols="30" value={details.concern} rows="10"></textarea></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td><button>Submit</button></td>
-        </tr>
-      </table>
-      </form> 
+      <form method="post" onSubmit={handleSubmit}>
+        <table className="whole">
+          <tr>
+            <td>
+              <label htmlFor="username">Username</label>
+            </td>
+            <td>
+              <input
+                type="text"
+                id="username"
+                onChange={handleInputChange}
+                placeholder="Enter your username"
+                name="username"
+                value={details.username}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label htmlFor="gmail">Gmail</label>
+            </td>
+            <td>
+              <input
+                type="gmailt"
+                id="gmail"
+                onChange={handleInputChange}
+                placeholder="Enter your gmail"
+                name="gmail"
+                value={details.gmail}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label htmlFor="contact">Contact Number</label>
+            </td>
+            <td>
+              <input
+                type="number"
+                id="contact"
+                onChange={handleInputChange}
+                placeholder="Enter your contact number"
+                name="number"
+                value={details.number}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label htmlFor="concern">Concern</label>
+            </td>
+            <td>
+              <textarea
+                name="concern"
+                id="concern"
+                onChange={handleInputChange}
+                cols="30"
+                value={details.concern}
+                rows="10"
+              ></textarea>
+            </td>
+          </tr>
+          <tr className="submit">
+            <td></td>
+            <td>
+              <button>Submit</button>
+            </td>
+          </tr>
+        </table>
+      </form>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,59 +1,120 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Navbar.css'
-import { Dropdown, DropdownTrigger, DropdownMenu, Button, DropdownSection, DropdownItem, Input } from "@nextui-org/react";
-import {SearchIcon} from "./utils/SearchIcon";
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  Button,
+  DropdownSection,
+  DropdownItem,
+  Input,
+} from "@nextui-org/react";
+import { SearchIcon } from "./utils/SearchIcon";
 const Navbar = () => {
   return (
     <>
       <header>
-        <nav className='navbar'>
-          <div id="logo"><Link to="/">Books.Rent</Link></div>
-
-
+        <nav className="navbar main">
+          <div id="logo">
+            <Link to="/" className="book">
+              Books.Rent
+            </Link>
+          </div>
           <div className="nav-links">
-            <Link><p href="/" className="">Books</p></Link>
+            <Link>
+              <p href="/" className="">
+                Books
+              </p>
+            </Link>
             <Dropdown backdrop="blur">
               <DropdownTrigger>
-                <Button variant="" className='nav-btn'>
+                <Button variant="" className="nav-btn">
                   <p>Categories</p>
                 </Button>
               </DropdownTrigger>
               <DropdownMenu variant="faded" aria-label="Static Actions">
-                <DropdownItem key="new"><Link className="dropdown-item" to="/Finance">Finance</Link></DropdownItem>
-                <DropdownItem key="copy"><Link className="dropdown-item" to="/SkillBased">Skill-Based</Link></DropdownItem>
-                <DropdownItem key="edit"><Link className="dropdown-item" to="/Biography">Biography</Link></DropdownItem>
-                <DropdownItem ><Link className="dropdown-item" to="/SelfHelp">Self-Help</Link></DropdownItem>
-                <DropdownItem key="delete" className="text-danger" color="danger">
-                <Link className="dropdown-item" to="/Fictional">Fictional</Link>
+                <DropdownItem key="new">
+                  <Link className="dropdown-item" to="/Finance">
+                    Finance
+                  </Link>
+                </DropdownItem>
+                <DropdownItem key="copy">
+                  <Link className="dropdown-item" to="/SkillBased">
+                    Skill-Based
+                  </Link>
+                </DropdownItem>
+                <DropdownItem key="edit">
+                  <Link className="dropdown-item" to="/Biography">
+                    Biography
+                  </Link>
+                </DropdownItem>
+                <DropdownItem>
+                  <Link className="dropdown-item" to="/SelfHelp">
+                    Self-Help
+                  </Link>
+                </DropdownItem>
+                <DropdownItem
+                  key="delete"
+                  className="text-danger"
+                  color="danger"
+                >
+                  <Link className="dropdown-item" to="/Fictional">
+                    Fictional
+                  </Link>
                 </DropdownItem>
               </DropdownMenu>
             </Dropdown>
-            
-
-            <Link><p>Wishlist</p></Link>&nbsp;
-            <Link><p>About Us</p></Link>
+            <Link>
+              <p>Wishlist</p>
+            </Link>
+            {/* &nbsp; */}
+            <Link>
+              <p className="about">About Us</p>
+            </Link>
             <Dropdown backdrop="blur">
               <DropdownTrigger>
-                <Button variant="" className='nav-btn'>
-                <p>Ways to Contact</p>
+                <Button variant="" className="nav-btn">
+                  <p>Ways to Contact</p>
                 </Button>
               </DropdownTrigger>
               <DropdownMenu variant="faded" aria-label="Static Actions">
-                <DropdownItem key="new"><Link className="dropdown-item" to="/Login"><p>Login</p></Link></DropdownItem>
-                <DropdownItem key="copy"><Link className="dropdown-item" to="/Signup"><p>Signup</p></Link></DropdownItem>
-                <DropdownItem key="edit"><Link className="dropdown-item" to="/feedback"><p>Feedback</p></Link></DropdownItem>
-                <DropdownItem key="edit"><Link className="dropdown-item" to="/suggestion"><p>Suggestion</p></Link></DropdownItem>
+                <DropdownItem key="new">
+                  <Link className="dropdown-item" to="/Login">
+                    Login
+                  </Link>
+                </DropdownItem>
+                <DropdownItem key="copy">
+                  <Link className="dropdown-item" to="/Signup">
+                    Signup
+                  </Link>
+                </DropdownItem>
+                <DropdownItem key="edit">
+                  <Link className="dropdown-item" to="/feedback">
+                    Feedback
+                  </Link>
+                </DropdownItem>
+                <DropdownItem key="edit">
+                  <Link className="dropdown-item" to="/suggestion">
+                    Suggestion
+                  </Link>
+                </DropdownItem>
 
-                <DropdownItem key="delete" className="text-danger" color="danger">
-                <Link className="dropdown-item" to="/Contact"><p>Contact</p></Link>
+                <DropdownItem
+                  key="delete"
+                  className="text-danger"
+                  color="danger"
+                >
+                  <Link className="dropdown-item" to="/Contact">
+                    Contact
+                  </Link>
                 </DropdownItem>
               </DropdownMenu>
             </Dropdown>
-            
           </div>
           <div>
-          <Input
+            <Input
               isClearable
               type="test"
               // label="Search"
@@ -61,16 +122,14 @@ const Navbar = () => {
               placeholder="Search For Books!"
               defaultValue=""
               onClear={() => console.log("input cleared")}
-              startContent = <SearchIcon/>
+              startContent=<SearchIcon />
               className="w-42 h-10 "
             />
           </div>
-          
-
         </nav>
       </header>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
