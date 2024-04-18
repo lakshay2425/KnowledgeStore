7e0bb90f59dcbdded533bef8972936283e2e5037
VIKAS GULIA <153659071+VIKASGULIA17@users.noreply.github.com>
2024-04-18 18:00:00 +0530

    homepage heading

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import './Home.css'
-import '../Categories/BestSellers/BestSellers.jsx'
-import BestSellers from '../Categories/BestSellers/BestSellers.jsx'
+import React from "react";
+import "./Home.css";
+import "../Categories/BestSellers/BestSellers.jsx";
+import BestSellers from "../Categories/BestSellers/BestSellers.jsx";
 const Home = () => {
   return (
     <>
-    <div className="home-section">
-      <div className="home-title">
-
+      <div className="home-section">
+        <div className="home-title">
+          <div className="home-heading">
+            <div>
+              <span>Find Your </span> Next Book
+            </div>
+          </div>
+          <div className="home-reading">
+            Our most popular and trending <span>On.Book</span> perfect
+          </div>
+          <div>Not sure what to read now next reading mood perfectly</div>
+        </div>
+        <div className="home-carousell"></div>
       </div>
-      <div className="home-carousell">
-        
-      </div>
-    </div>
-    <BestSellers/>
+      <BestSellers />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
