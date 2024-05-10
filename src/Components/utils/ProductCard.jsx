a684112d0a52ca2f6eaebaeaceb0c943a57b99c9
Rohan Kumar Singh <rohankrsingh125@gmail.com>
2024-05-10 22:57:15 +0530

    fixed product card

--- src/Components/utils/ProductCard.jsx
+++ src/Components/utils/ProductCard.jsx
@@ -1,22 +1,35 @@
 import React from "react";
-
+import { Image } from "@nextui-org/react";
+import { IoBagHandleOutline } from "react-icons/io5";
 const ProductCard = ({ books }) => {
   const img = "https://bestlifeonline.com/wp-content/uploads/sites/3/2020/10/The-Hobbit-book-cover.jpg";
   return (
-    <>   
-         {books.map((book) => (
-         <div key={book.s_no} className="product-card">
-        <img src= {book.img_link} alt= {book.book_name} className="product-img" />
-        <div className="product-card-chip">
-          <span className="product-name">{book.book_name}</span>
-          <span className="product-price">&#8377;{book.price}</span>
+    <>
+      {books.map((book) => (
+        <div key={book.s_no} className="product-card">
+          <Image
+            className="product-img"
+            
+            isZoomed
+            width={280}
+            height={525}
+            alt={book.book_name}
+            src={book.img_link}
+            style={{opacity: 100}}
+          />
+          <div className="product-card-chip">
+            <span className="product-name">{book.book_name}</span>
+            <span className="product-price">&#8377;{book.price}</span>
+          </div>
+          <div className="product-card-chip">
+            <span className="product-author">By {book.author}</span>
+            <span className="genre">{book.genre}</span>
+          </div>
+          
+          {/* <p className="product-total-items">Total Items: {totalItems}</p> */}
+          <button className="product-btn">Add <IoBagHandleOutline /></button>
         </div>
-        <span className="product-author">By {book.author}</span>
-        <span className="card-text">Genre: {book.genre}</span>
-        {/* <p className="product-total-items">Total Items: {totalItems}</p> */}
-        <button className="product-btn">Add to Cart</button>
-      </div>
-      ))} 
+      ))}
     </>
 
     // <div className="product-card">
