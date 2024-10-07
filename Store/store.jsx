2a514266a96b80834ae810a9c47de3127322009c
Lakshay Mahajan <lakshay12290@gmail.com>
2024-10-07 20:17:56 +0530

    Fixed a bug in authentication system

--- Store/store.jsx
+++ Store/store.jsx
@@ -1,15 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import alertReducers from "../features/alertSlice"
 import bookDetailsSlice from '../features/bookDetailsSlice';
-import loginAuthSlice from '../features/loginAuthSlice';
-
 
 // Configure and export the store
 const store = configureStore({
   reducer: {
     alert : alertReducers,
     book : bookDetailsSlice,
-    auth : loginAuthSlice
   },
 });
 
