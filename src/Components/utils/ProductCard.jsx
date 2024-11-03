be282ffc2b0cd0276090b885ed1f65ac4cef4c6d
Rohan Kumar Singh <rohankrsingh125@gmail.com>
2024-11-03 11:04:27 +0530

    "Refactor code in Recomended, Home, ProductCard components and index.css file"

--- src/Components/utils/ProductCard.jsx
+++ src/Components/utils/ProductCard.jsx
@@ -70,15 +70,15 @@ const ProductCard = ({ books }) => {
   return (
     <>
       {books.map((book) => (
-        <div key={book._id} className="product-card max-md:w-52 rounded-[--nextui-radius-large] shadow-small px-4 py-3 m-2 backdrop-blur-lg max-md:p-2 z-0 max-sm:w-48 max-sm: max-lg:w-60">
+        <div key={book._id} className=" rounded-[--nextui-radius-large] shadow-small px-4 py-3 m-2 backdrop-blur-lg max-md:p-2 z-0 max-md:w-52 max-sm:w-44 max-lg:w-60">
           <Image
             onClick={() => Preview(book)}
-            className="max-w-full  h-[450px] max-md:h-80 max-md:max-w-48 max-sm:w-44 max-xl:h-[300px] max-lg:h-80"
+            className="max-w-full  h-[450px] max-md:h-80 max-md:max-w-48 max-sm:w-40 max-sm:h-64 max-xl:h-[300px] max-lg:h-80 max-lg:w-52"
             alt={book.title}
             src={book.imageLink}
           />
           <div className="product-card-chip">
-            <div className="product-name max-md:text-sm max-sm:text-xs max-xl:text-small" >
+            <div className="product-name max-md:text-sm max-sm:text-xs max-xl:text-small max-lg:" >
               <span className="slide">{book.title}</span>
 
             </div>
