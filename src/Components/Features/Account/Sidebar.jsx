c64c40465b37e3f7530c7b6a4e534b91e23c4d89
Rohan Kumar Singh <rohankrsingh125@gmail.com>
2024-11-06 18:53:27 +0530

    "Added DeleteUser component to Sidebar and updated menu items"

--- src/Components/Features/Account/Sidebar.jsx
+++ src/Components/Features/Account/Sidebar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import DeleteUser from './DeleteUser';
 
 const Sidebar = () => {
     const menuItems = [
         "My profile",
         "Wishlist",
-        "Orders"
+        "Orders",
+        
     ];
     return (
         <ul className="bg-slate-200/60 w-full md:w-48 lg:w-64 h-auto px-4 md:px-6 lg:px-8 py-6 text-zinc-600 text-base md:text-lg capitalize">
@@ -16,9 +18,10 @@ const Sidebar = () => {
                     <a href="#">{item}</a>
                 </li>
             ))}
-            <li className="text-red-600 list-none hover:bg-white rounded-full pl-4 py-3 hover:font-medium w-full md:w-40 mt-9">
-                <a href="#">Delete Account</a>
+            <li className='list-none rounded-full pl-4 hover:font-medium duration-500 md:pl-5 py-2 md:py-3 text-base md:text-lg capitalize'>
+                <DeleteUser/>
             </li>
+            
         </ul>
     );
 };
