58069841b4f70e35412e7a1d4bfd448ecb9529ad
Lakshay Mahajan <lakshay12290@gmail.com>
2024-11-27 11:28:15 +0530

    Created a utily function to remove all product from user cart or wishlist

---
+++ src/Components/utils/deleteAllProducts.js
@@ -0,0 +1,14 @@
+import axiosInstance from "./Axios";
+
+export const deleteProducts = async (email, type) => {
+    try {
+        if (type === "wishlist") {
+            const response = await axiosInstance.delete(`${import.meta.env.VITE_BACKEND_URL}/${type}/deleteAll?email=${email}`);
+        }else if(type === "cart"){
+            const response = await axiosInstance.delete(`${import.meta.env.VITE_BACKEND_URL}/${type}/deleteAll?email=${email}`);
+        }
+        console.log("All products deleted successfully");
+    } catch (error) {
+        console.error(error.message);
+    }
+}
